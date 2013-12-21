221e1e7984b2200d341382a5abc2c27217ecdadf
Grief-Code <hasgrief@googlemail.com>
2013-12-21 08:50:51 +0000

    Fix #127

--- ghost++/update_stats/update_stats.cpp
+++ ghost++/update_stats/update_stats.cpp
@@ -327,7 +327,7 @@ int main( int argc, char **argv )
                 uint32_t leaver[11];
                 uint32_t npoints[11];
                 string points[11];
-                uint32_t ingame_role[11] = 0;
+                uint32_t ingame_role[11];
 
                 vector<string> Row = MySQLFetchRow( Result );
 
