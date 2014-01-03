7d57cc2bd77304c016f6276bd3d7d0d3a9cdf597
Grief-Code <hasgrief@googlemail.com>
2014-01-03 14:10:02 +0000

    Just copyright notice

--- ghost++/ghost/crc32.cpp
+++ ghost++/ghost/crc32.cpp
@@ -1,3 +1,24 @@
+/**
+  * Copyright [2013-2014] [OHsystem]
+  * 
+  * OHSystem is free software: You can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  * 
+  * Please save the copyrights and notifications on the footer.
+  * 
+  * You can contact the developers on: ohsystem-public@googlegroups.com
+  * or join us directly here: https://groups.google.com/d/forum/ohsystem-public
+  * 
+  * Visit us also on http://ohsystem.net/ and keep track always of the latest
+  * features and changes.
+  * 
+  * 
+  * This is modified from GHOST++: http://ghostplusplus.googlecode.com/
+  * Official GhostPP-Forum: http://ghostpp.com/
+ */
+
 #include "ghost.h"
 #include "crc32.h"
 
