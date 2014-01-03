e4c325e1539206bd42518108b953980839739482
Ivan <ivan.anta@gmail.com>
2014-01-03 14:40:34 +0100

    Just copyright notice
    Added copyright notice.

--- openstats/inc/common.php
+++ openstats/inc/common.php
@@ -1,42 +1,26 @@
 <?php
-/*********************************************
-<!-- 
-*   	DOTA OPENSTATS
-*   <?php
-/*********************************************
-<!-- 
-*   	DOTA OPENSTATS
-*   
-*	Developers: Ivan.
-*	Contact: ivan.anta@gmail.com - Ivan
-*
-*	
-*	Please see http://openstats.iz.rs
-*	and post your webpage there, so I know who's using it.
-*
-*	Files downloaded from http://openstats.iz.rs
-*
-*	Copyright (C) 2010  Ivan
-*
-*
-*	This file is part of DOTA OPENSTATS.
-*
-* 
-*	 DOTA OPENSTATS is free software: you can redistribute it and/or modify
-*    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation, either version 3 of the License, or
-*    (at your option) any later version.
-*
-*    DOTA OPEN STATS is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU General Public License for more details.
-*
-*    You should have received a copy of the GNU General Public License
-*    along with DOTA OPEN STATS.  If not, see <http://www.gnu.org/licenses/>
-*
--->
-**********************************************/
+/**
+ * Copyright [2013-2014] [OHsystem]
+ * 
+ * OHSystem is free software: You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Please save the copyrights and notifications on the footer.
+ * This file is part of DOTA OPENSTATS.
+ * 
+ * You can contact the developers on: ohsystem-public@googlegroups.com
+ * or join us directly here: https://groups.google.com/d/forum/ohsystem-public
+ * 
+ * Visit us also on http://ohsystem.net/ and keep track always of the latest
+ * features and changes.
+ * 
+ * 
+ * This is modified from GHOST++: http://ghostplusplus.googlecode.com/
+ * Official GhostPP-Forum: http://ghostpp.com/
+*/
+
 if (!isset($website) ) {header('HTTP/1.1 404 Not Found'); die; }
 if (!isset($_SESSION["level"]) AND empty($_SESSION["level"]) ) $_SESSION["level"] = 0;
 $errors = "";
--- openstats/load_os.php
+++ openstats/load_os.php
@@ -1,4 +1,26 @@
 <?php
+/**
+ * Copyright [2013-2014] [OHsystem]
+ * 
+ * OHSystem is free software: You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Please save the copyrights and notifications on the footer.
+ * This file is part of DOTA OPENSTATS.
+ * 
+ * You can contact the developers on: ohsystem-public@googlegroups.com
+ * or join us directly here: https://groups.google.com/d/forum/ohsystem-public
+ * 
+ * Visit us also on http://ohsystem.net/ and keep track always of the latest
+ * features and changes.
+ * 
+ * 
+ * This is modified from GHOST++: http://ghostplusplus.googlecode.com/
+ * Official GhostPP-Forum: http://ghostpp.com/
+*/
+
 if (strstr($_SERVER['REQUEST_URI'], basename(__FILE__) ) ) {header('HTTP/1.1 404 Not Found'); die; }
 
   $time = microtime();
