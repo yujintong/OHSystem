190b9df14093b1706773fe1512d8c5aa37334d1b
Grief-Code <hasgrief@googlemail.com>
2014-01-06 23:09:42 +0000

    Minor fixes

--- ghost++/ghost/ghost.cpp
+++ ghost++/ghost/ghost.cpp
@@ -2071,7 +2071,7 @@ void CGHost :: ReadRoomData()
 }
 
 string CGHost :: GetAliasName( uint32_t alias ) {
-    if( m_Aliases.size( ) != 0 && m_Aliases.size( ) >= alias ) {
+    if( m_Aliases.size( ) != 0 && m_Aliases.size( ) >= alias && alias != 0 ) {
         return m_Aliases[alias-1];
     }
     return "failed";
