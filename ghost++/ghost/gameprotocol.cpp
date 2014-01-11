6a258a25281b3b68c3ac8d1186c28f28fcdf41cd
Grief-Code <hasgrief@googlemail.com>
2014-01-11 08:39:12 +0000

    Last update on copyright notice

--- ghost++/ghost/gameprotocol.cpp
+++ ghost++/ghost/gameprotocol.cpp
@@ -1,24 +1,25 @@
 /**
-  * Copyright [2013-2014] [OHsystem]
-  * 
-  * OHSystem is free software: You can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation, either version 3 of the License, or
-  * (at your option) any later version.
-  * 
-  * Please save the copyrights and notifications on the footer.
-  * We do NOT support or help people who removed this notice.
-  * 
-  * You can contact the developers on: admin@ohsystem.net
-  * or join us directly here: https://ohsystem.net/forum
-  * 
-  * Visit us also on http://ohsystem.net/ and keep track always of the latest
-  * features and changes.
-  * 
-  * This is modified from GHOST++: http://ghostplusplus.googlecode.com/
-  * Official GhostPP-Forum: http://ghostpp.com/
- */
-
+* Copyright [2013-2014] [OHsystem]
+*
+* We spent a lot of time writing this code, so show some respect:
+* - Do not remove this copyright notice anywhere (bot, website etc.)
+* - We do not provide support to those who removed copyright notice
+*
+* OHSystem is free software: You can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* You can contact the developers on: admin@ohsystem.net
+* or join us directly here: http://ohsystem.net/forum/
+*
+* Visit us also on http://ohsystem.net/ and keep track always of the latest
+* features and changes.
+*
+*
+* This is modified from GHOST++: http://ghostplusplus.googlecode.com/
+* Official GhostPP-Forum: http://ghostpp.com/
+*/
 #include "ghost.h"
 #include "util.h"
 #include "crc32.h"
