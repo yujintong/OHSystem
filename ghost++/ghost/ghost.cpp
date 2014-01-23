11c8949e86501ddf53a567c9f791366e17956e12
Grief-Code <hasgrief@gmail.com>
2014-01-23 19:19:01 +0000

    Fixed an issue on the virtual lobby.

--- ghost++/ghost/ghost.cpp
+++ ghost++/ghost/ghost.cpp
@@ -1574,7 +1574,7 @@ void CGHost :: SetConfigs( CConfig *CFG )
     m_DenieCountriesOnThisBot = CFG->GetInt( "oh_deniedcountries", 1 ) == 0 ? false : true;
     m_KickSlowDownloader = CFG->GetInt("oh_kickslowdownloader", 1 ) == 0 ? false :  true;
     m_VirtualLobby = CFG->GetInt("oh_virtuallobby", 1 ) == 0 ? false : true;
-    m_VirtualLobbyTime = CFG->GetInt("oh_virtuallobbytime", 10);
+    m_VirtualLobbyTime = CFG->GetInt("oh_virtuallobbytime", 20);
     m_CustomVirtualLobbyInfoBanText = CFG->GetString("oh_virtuallobbybantext", string( ));
 }
 
