744846c2ef4c1dd7fa6ee31f5179d8266f685e03
Grief-Code <hasgrief@googlemail.com>
2014-01-24 09:49:49 +0000

    Small update to display bans on virtual lobby.

--- ghost++/ghost/game_base.cpp
+++ ghost++/ghost/game_base.cpp
@@ -2038,18 +2038,22 @@ void CBaseGame :: SendVirtualLobbyInfo( CPotentialPlayer *player, CDBBan *Ban, u
     player->GetSocket( )->PutBytes( m_Protocol->SEND_W3GS_MAPCHECK( m_Map->GetMapPath( ), m_Map->GetMapSize( ), m_Map->GetMapInfo( ), m_Map->GetMapCRC( ), m_Map->GetMapSHA1( ) ) );
     player->GetSocket( )->PutBytes( m_Protocol->SEND_W3GS_CHAT_FROM_HOST( 1, UTIL_CreateByteArray( 2 ), 16, BYTEARRAY( ), "You are currently unable to join the game." ) );
     if(1==type) {
-        player->GetSocket( )->PutBytes( m_Protocol->SEND_W3GS_CHAT_FROM_HOST( 1, UTIL_CreateByteArray( 2 ), 16, BYTEARRAY( ), "You are banned!" ) );
+        player->GetSocket( )->PutBytes( m_Protocol->SEND_W3GS_CHAT_FROM_HOST( 1, UTIL_CreateByteArray( 2 ), 16, BYTEARRAY( ), ">>>>>> You are banned! <<<<<<" ) );
         player->GetSocket( )->PutBytes( m_Protocol->SEND_W3GS_CHAT_FROM_HOST( 1, UTIL_CreateByteArray( 2 ), 16, BYTEARRAY( ), "       Name:   " + Ban->GetName() + "@" + Ban->GetServer()) );
         string Remain = "";
         if( Ban->GetMonths() != "0" )
-            Remain += Ban->GetMonths() +"month(s), ";
+            Remain += Ban->GetMonths() +"m, ";
         if( Ban->GetDays() != "0" )
-            Remain += Ban->GetDays() +"day(s), ";
+            Remain += Ban->GetDays() +"d, ";
         if( Ban->GetHours() != "0" )
-            Remain += Ban->GetHours() +"hour(s), ";
+            Remain += Ban->GetHours() +"h, ";
         if( Ban->GetMinutes() != "0" )
-            Remain += Ban->GetMinutes() +"minute(s), ";
-        player->GetSocket( )->PutBytes( m_Protocol->SEND_W3GS_CHAT_FROM_HOST( 1, UTIL_CreateByteArray( 2 ), 16, BYTEARRAY( ), "       Date:   " + Ban->GetDate() + " (Remain: " + Remain + ")" ) );
+            Remain += Ban->GetMinutes() +"m ";
+        if(! Remain.empty() )
+            player->GetSocket( )->PutBytes( m_Protocol->SEND_W3GS_CHAT_FROM_HOST( 1, UTIL_CreateByteArray( 2 ), 16, BYTEARRAY( ), "       Date:   " + Ban->GetDate() + " (Remain: " + Remain + ")" ) );
+        else
+            player->GetSocket( )->PutBytes( m_Protocol->SEND_W3GS_CHAT_FROM_HOST( 1, UTIL_CreateByteArray( 2 ), 16, BYTEARRAY( ), "       Date:   " + Ban->GetDate() + " (Permanently Banned!") );
+
         player->GetSocket( )->PutBytes( m_Protocol->SEND_W3GS_CHAT_FROM_HOST( 1, UTIL_CreateByteArray( 2 ), 16, BYTEARRAY( ), "       Reason: " + Ban->GetReason( ) ) );
         player->GetSocket( )->PutBytes( m_Protocol->SEND_W3GS_CHAT_FROM_HOST( 1, UTIL_CreateByteArray( 2 ), 16, BYTEARRAY( ), m_GHost->m_CustomVirtualLobbyInfoBanText ) );
     } else if(2==type) {
