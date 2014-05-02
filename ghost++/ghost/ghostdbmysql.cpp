3381e34094ae2673e2967dfafc08adcff1eba80d
Grief-Code <hasgrief@googlemail.com>
2014-05-02 14:16:12 +0100

    Missed ot remove this

--- ghost++/ghost/ghostdbmysql.cpp
+++ ghost++/ghost/ghostdbmysql.cpp
@@ -2825,10 +2825,6 @@ bool MySQLW3MMDVarAdd( void *conn, string *error, uint32_t botid, uint32_t gamei
 
 bool MySQLBotStatusCreate( void *conn, string *error, uint32_t botid, string username, string gamename, string ip, uint16_t hostport, string roc, string tft )
 {
-    string Query = "DELTE FROM oh_bot_status WHERE botid='"+UTIL_ToString(botid)+"'";
-    if( mysql_real_query( (MYSQL *)conn, Query.c_str( ), Query.size( ) ) != 0 )
-        *error = mysql_error( (MYSQL *)conn );
-
     string InsertNow = "INSERT INTO oh_bot_status (botid, name, gamename, ip, hostport, roc, tft, last_update) VALUES ('"+UTIL_ToString(botid)+"', '"+username+"','"+gamename+"', '"+ip+"','"+UTIL_ToString(hostport)+"','"+roc+"','"+tft+"', NOW()) ON DUPLICATE KEY UPDATE name='"+username+"',gamename='"+gamename+"',ip='"+ip+"',hostport='"+UTIL_ToString(hostport)+"', roc='"+roc+"', tft='"+tft+"', last_update=NOW()";
     mysql_real_query( (MYSQL *)conn, InsertNow.c_str( ), InsertNow.size( ) );
 
