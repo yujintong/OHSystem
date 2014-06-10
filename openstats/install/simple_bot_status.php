a26654626747e9d6b9462ac2452c2dd21a2d16cf
Grief-Code <hasgrief@googlemail.com>
2014-06-10 20:10:02 +0100

    Using tiles instead of a list. Page is minimal responsive now.

--- openstats/install/simple_bot_status.php
+++ openstats/install/simple_bot_status.php
@@ -2,8 +2,8 @@
 <head>
 <title>Bot Status</title>
 <style>
+<style>
 body {
-    width: 600px;
     margin-top: 20px;
     margin-left: 40px;
     font-family: 'trebuchet MS', 'Lucida sans', Arial;
@@ -11,86 +11,12 @@ body {
     color: #444;
 }
 
-table {
-    *border-collapse: collapse; /* IE7 and lower */
-    border-spacing: 0;
-    width: 100%;
-}
+.bot { display: inline-block; float: left; width: 10%; min-width: 250px; height: 215px; color: #fff; border: 1px solid #222; border-bottom: 1px solid #aaa; background-color: #088A08; margin: 10px; padding: 5px; font-size: 90%;}
+.bot.warning { background-color: #D7DF01; color: #000;}
+.bot.danger { background-color: #DF7401}
+.bot.error { background-color: #B40404}
 
-.bordered {
-    border: solid #ccc 1px;
-    -moz-border-radius: 6px;
-    -webkit-border-radius: 6px;
-    border-radius: 6px;
-    -webkit-box-shadow: 0 1px 1px #ccc;
-    -moz-box-shadow: 0 1px 1px #ccc;
-    box-shadow: 0 1px 1px #ccc;
-}
-
-.bordered tr:hover {
-    background: #fbf8e9;
-    -o-transition: all 0.1s ease-in-out;
-    -webkit-transition: all 0.1s ease-in-out;
-    -moz-transition: all 0.1s ease-in-out;
-    -ms-transition: all 0.1s ease-in-out;
-    transition: all 0.1s ease-in-out;
-}
-
-.bordered td, .bordered th {
-    border-left: 1px solid #ccc;
-    border-top: 1px solid #ccc;
-    padding: 10px;
-    text-align: left;
-}
-
-.bordered th {
-    background-color: #dce9f9;
-    background-image: -webkit-gradient(linear, left top, left bottom, from(#ebf3fc), to(#dce9f9));
-    background-image: -webkit-linear-gradient(top, #ebf3fc, #dce9f9);
-    background-image:    -moz-linear-gradient(top, #ebf3fc, #dce9f9);
-    background-image:     -ms-linear-gradient(top, #ebf3fc, #dce9f9);
-    background-image:      -o-linear-gradient(top, #ebf3fc, #dce9f9);
-    background-image:         linear-gradient(top, #ebf3fc, #dce9f9);
-    -webkit-box-shadow: 0 1px 0 rgba(255,255,255,.8) inset;
-    -moz-box-shadow:0 1px 0 rgba(255,255,255,.8) inset;
-    box-shadow: 0 1px 0 rgba(255,255,255,.8) inset;
-    border-top: none;
-    text-shadow: 0 1px 0 rgba(255,255,255,.5);
-}
-
-.bordered td:first-child, .bordered th:first-child {
-    border-left: none;
-}
-
-.bordered th:first-child {
-    -moz-border-radius: 6px 0 0 0;
-    -webkit-border-radius: 6px 0 0 0;
-    border-radius: 6px 0 0 0;
-}
-
-.bordered th:last-child {
-    -moz-border-radius: 0 6px 0 0;
-    -webkit-border-radius: 0 6px 0 0;
-    border-radius: 0 6px 0 0;
-}
-
-.bordered th:only-child{
-    -moz-border-radius: 6px 6px 0 0;
-    -webkit-border-radius: 6px 6px 0 0;
-    border-radius: 6px 6px 0 0;
-}
-
-.bordered tr:last-child td:first-child {
-    -moz-border-radius: 0 0 0 6px;
-    -webkit-border-radius: 0 0 0 6px;
-    border-radius: 0 0 0 6px;
-}
-
-.bordered tr:last-child td:last-child {
-    -moz-border-radius: 0 0 6px 0;
-    -webkit-border-radius: 0 0 6px 0;
-    border-radius: 0 0 6px 0;
-}
+</style>
 </style>
 </head>
 <body>
@@ -101,31 +27,33 @@ mysql_connect("localhost", "root", "YOUR_PASSWORD");
 mysql_select_db("ohsystem");
 
 $result = mysql_query("SELECT * FROM oh_bot_status WHERE last_update >= addtime(now(), '-00:01:00')");
+
+while($row = mysql_fetch_array($result)) {
+ $count = 0;
+ if($row['Europe']!=1)
+  $count++;
+ if($row['USEast']!=1)
+  $count++;
+ if($row['USWest']!=1)
+  $count++;
+ if($row['Asia']!=1)
+  $count++;
+ if($row['XAMP']!=1)
+  $count++;
+ if($row['Garena']!=1)
+  $count++;
+ if($row['WC3Connect']!=1)
+  $count++;
 ?>
-<table class="bordered">
- <thead>
-        <th>BotID</th>
-        <th>Bot Name</th>
-        <th style="width:220px;">GameName</th>
-        <th>IP</th>
-        <th>HostPort</th>
-        <th>Europe</th>
-        <th>USEast</th>
-        <th>USWest</th>
-        <th>Asia</th>
-        <th>XAMP</th>
-        <th>Garena</th>
-        <th>WC3Connect</th>
- </thead>
- <tbody>
-<?php
-while($row = mysql_fetch_array($result)) { ?>
- <tr><td><?=$row['botid']?></td><td><?=$row['name']?></td><td><?=$row['gamename']?></td><td><?=$row['ip']?></td><td><?=$row['hostport']?></td>
- <td><?=$row['Europe']?></td><td><?=$row['USEast']?></td><td><?=$row['USWest']?></td><td><?=$row['Asia']?></td><td><?=$row['XAMP']?></td><td><?=$row['Garena']?></td><td><?=$row['WC3Connect']?></td>
- </tr>
+ <div class="bot <?php echo ( $count != 0 ? ($count < 2 ? 'warning' : ($count < 3 ? 'danger' : 'error' ) ) : '' ) ?>"><b>Bot ID:</b> #<?=$row['botid']?><br><b>Username: </b><?=$row['name']?><br><b>GameName: </b><?=$row['gamename']?><br>
+  <b>Hosted IP:Port: </b><?=$row['ip']?>:<?=$row['hostport']?><br><br>
+
+  <b>Europe: </b><?=$row['Europe']?><br><b>USEast: </b><?=$row['USEast']?><br><b>USWest: </b><?=$row['USWest']?><br><b>Asia: </b><?=$row['Asia']?><br><b>XAMP: </b><?=$row['XAMP']?><br><b>Garena: </b><?=$row['Garena']?><br>
+  <b>WC3Connect: </b><?=$row['WC3Connect']?>
+ </div>
 <? } ?>
-</tbody>
-</table>
+<div style="clear: both"></div>
+<br>
 
 Bot Status:<br>
 0 - Not initalized<br>
