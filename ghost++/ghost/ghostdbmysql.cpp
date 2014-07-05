dc1ec129685b4265df8f24313ebdf1b9de69a404
Grief-Code <hasgrief@googlemail.com>
2014-07-05 16:06:34 +0100

    Close #285

--- ghost++/ghost/ghostdbmysql.cpp
+++ ghost++/ghost/ghostdbmysql.cpp
@@ -2829,17 +2829,20 @@ bool MySQLW3MMDVarAdd( void *conn, string *error, uint32_t botid, uint32_t gamei
 
 bool MySQLBotStatusCreate( void *conn, string *error, uint32_t botid, string username, string gamename, string ip, uint16_t hostport, string roc, string tft )
 {
-    string InsertNow = "INSERT INTO oh_bot_status (botid, name, gamename, ip, hostport, roc, tft, last_update) VALUES ('"+UTIL_ToString(botid)+"', '"+username+"','"+gamename+"', '"+ip+"','"+UTIL_ToString(hostport)+"','"+roc+"','"+tft+"', NOW()) ON DUPLICATE KEY UPDATE name='"+username+"',gamename='"+gamename+"',ip='"+ip+"',hostport='"+UTIL_ToString(hostport)+"', roc='"+roc+"', tft='"+tft+"', last_update=NOW()";
-    mysql_real_query( (MYSQL *)conn, InsertNow.c_str( ), InsertNow.size( ) );
-
+    if(server == "europe.battle.net" || server == "uswest.battle.net" || server == "useast.battle.net" ||server == "asia.battle.net" || server == "server.eurobattle.net") {
+        string InsertNow = "INSERT INTO oh_bot_status (botid, name, gamename, ip, hostport, roc, tft, last_update) VALUES ('"+UTIL_ToString(botid)+"', '"+username+"','"+gamename+"', '"+ip+"','"+UTIL_ToString(hostport)+"','"+roc+"','"+tft+"', NOW()) ON DUPLICATE KEY UPDATE name='"+username+"',gamename='"+gamename+"',ip='"+ip+"',hostport='"+UTIL_ToString(hostport)+"', roc='"+roc+"', tft='"+tft+"', last_update=NOW()";
+        mysql_real_query( (MYSQL *)conn, InsertNow.c_str( ), InsertNow.size( ) );
+    }
     return 0;
 }
 
 bool MySQLBotStatusUpdate( void *conn, string *error, uint32_t botid, string server, uint32_t status )
 {
-    string Query = "UPDATE oh_bot_status SET "+server+" = '"+UTIL_ToString(status)+"', last_update=NOW() WHERE botid ="+UTIL_ToString(botid);
-    if( mysql_real_query( (MYSQL *)conn, Query.c_str( ), Query.size( ) ) != 0 )
-        *error = mysql_error( (MYSQL *)conn );
+    if(server == "europe.battle.net" || server == "uswest.battle.net" || server == "useast.battle.net" ||server == "asia.battle.net" || server == "server.eurobattle.net") {
+        string Query = "UPDATE oh_bot_status SET "+server+" = '"+UTIL_ToString(status)+"', last_update=NOW() WHERE botid ="+UTIL_ToString(botid);
+        if( mysql_real_query( (MYSQL *)conn, Query.c_str( ), Query.size( ) ) != 0 )
+            *error = mysql_error( (MYSQL *)conn );
+    }
     return 0;
 }
 
