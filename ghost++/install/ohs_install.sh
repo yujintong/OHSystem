c42819526c02c0b649484ea01fd0af69a628bec7
Grief-Code <admin@gcoding.net>
2014-07-15 14:14:03 +0200

    Update ohs_install.sh

--- ghost++/install/ohs_install.sh
+++ ghost++/install/ohs_install.sh
@@ -1,4 +1,4 @@
-apt-get install screen bzip2 tar build-essential g++ libmysql++-dev wget unzip libbz2-dev libgmp3-dev git
+apt-get -y install screen bzip2 tar build-essential g++ libmysql++-dev wget unzip libbz2-dev libgmp3-dev git
 cd /home
 wget http://downloads.sourceforge.net/project/boost/boost/1.54.0/boost_1_54_0.tar.bz2
 tar xjf boost_1_54_0.tar.bz2
