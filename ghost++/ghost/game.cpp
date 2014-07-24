8c205158656034a12b2d0f0cdfd9ecfa05ff59ec
Grief-Code <admin@gcoding.net>
2014-07-24 08:47:04 +0200

    Update game.cpp
    Fixed #297

--- ghost++/ghost/game.cpp
+++ ghost++/ghost/game.cpp
@@ -2044,18 +2044,7 @@ bool CGame :: EventPlayerBotCommand( CGamePlayer *player, string command, string
                         SendAllChat( m_GHost->m_Language->UnableToCheckPlayerNoMatchesFound( Payload ) );
                     else if( Matches == 1 )
                     {
-                        uint32_t CLevel = 0;
-                        string CLevelName;
-                        for( vector<CBNET *> :: iterator i = m_GHost->m_BNETs.begin( ); i != m_GHost->m_BNETs.end( ); ++i )
-                        {
-                            if( (*i)->GetServer( ) == player->GetSpoofedRealm( )  || player->GetSpoofedRealm( ) == m_GHost->m_WC3ConnectAlias )
-                            {
-                                CLevel = (*i)->IsLevel( LastMatch->GetName( ) );
-                                CLevelName = (*i)->GetLevelName( CLevel );
-                                break;
-                            }
-                        }
-                        SendAllChat( "[" + LastMatch->GetName( ) + "] (P: " + ( LastMatch->GetNumPings( ) > 0 ? UTIL_ToString( LastMatch->GetPing( m_GHost->m_LCPings ) ) + "ms" : "N/A" ) + ") (F: " + LastMatch->GetCLetter( )+ ") (Role: " + ( CLevelName.empty( ) ? "unknown" : CLevelName ) + ") (SpoofChecked: " + ( LastMatch->GetSpoofed( ) ? "Yes" : "No" ) + ") (Realm: " + ( LastMatch->GetSpoofedRealm( ).empty( ) ? "N/A" : LastMatch->GetSpoofedRealm( ) ) + ")" );
+                        SendAllChat( "[" + LastMatch->GetName( ) + "] (P: " + ( LastMatch->GetNumPings( ) > 0 ? UTIL_ToString( LastMatch->GetPing( m_GHost->m_LCPings ) ) + "ms" : "N/A" ) + ") (F: " + LastMatch->GetCLetter( )+ ") (Role: " + ( LastMatch->GetLevelName( ).empty() ? "unknown" : LastMatch->GetLevelName( ) ) + ") (SpoofChecked: " + ( LastMatch->GetSpoofed( ) ? "Yes" : "No" ) + ") (Realm: " + ( LastMatch->GetSpoofedRealm( ).empty( ) ? "N/A" : LastMatch->GetSpoofedRealm( ) ) + ")" );
                     }
                     else
                         SendAllChat( m_GHost->m_Language->UnableToCheckPlayerFoundMoreThanOneMatch( Payload ) );
