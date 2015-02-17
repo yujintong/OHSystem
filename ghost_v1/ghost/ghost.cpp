897c3b051f2cbe3c31e66847c15ef494bb0d26c1
Michael Schneider <admin@gcoding.net>
2015-02-17 10:20:16 +0100

    Update ghost.cpp
    Added small addition to keep track of outstanding callables

--- ghost_v1/ghost/ghost.cpp
+++ ghost_v1/ghost/ghost.cpp
@@ -1250,6 +1250,7 @@ bool CGHost :: Update( long usecBlock )
         m_AVGTicks = m_TicksCollection/m_Sampler;
         m_TicksCollectionTimer = GetTicks();
         CONSOLE_Print("[OHSystem-Performance-Check] AVGTicks: "+UTIL_ToString(m_AVGTicks, 0)+"ms | MaxTicks: "+UTIL_ToString(m_MaxTicks)+"ms | MinTicks: "+UTIL_ToString(m_MinTicks)+"ms | Updates: "+UTIL_ToString(m_Sampler));
+        CONSOLE_Print("[OHSystem-Performance-CHeck] MySQL: "+m_DB->GetStatus( ));
         m_MinTicks = -1;
         m_MaxTicks = 0;
         m_TicksCollection = 0;
